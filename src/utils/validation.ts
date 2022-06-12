983c2898da7bd31778dd971dfdf3b62ef14702f6
Nikita Iskorostinskii <nikita11042000@gmai.com>
2022-06-12 22:44:38 +0300

    refactor: updated regexps

--- src/utils/validation.ts
+++ src/utils/validation.ts
@@ -1,23 +1,23 @@
 export function validation(field: string, value: string): boolean {
   switch (field) {
     case 'login': {
-      return /^[a-zA-Z0-9_-]{3,20}$/.test(value);
+      return /^[a-zA-Z][a-zA-Z0-9]{4,20}$/.test(value);
     }
     case 'password': {
-      return /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,40}$/.test(value);
+      return /^[a-zA-Zа-яёА-ЯЁ0-9]{8,25}$/.test(value);
     }
     case 'email': {
-      return /^\\w+([.-]?\\w+)*@\\w+([.-]?\\w+)*(\\.\\w{2,3})+$/.test(value);
+      return /^[a-zA-Z0-9_.+-]{2,20}@[a-zA-Z0-9-]{2,20}\.[a-z]{2,10}$/.test(value);
     }
     case 'phone': {
-      return /^((\\+7|7|8)+([0-9]){10})$|\\b\\d{3}[-.]?\\d{3}[-.]?\\d{4}\b/.test(value);
+      return /^(\+{1}7{1}|8) ?\(?\d{3}\)? ?\d{3}[ -]?\d{2}[ -]?\d{2}$/.test(value);
     }
     case 'message': {
       return value !== '';
     }
     case 'first_name':
     case 'last_name': {
-      return /^([А-Я]{1}[а-яё]{1,23}|[A-Z]{1}[a-z]{1,23})$/.test(value);
+      return /^[a-zA-Zа-яёА-ЯЁ]{2,20}$/.test(value);
     }
     default: {
       return false;
